290b8e84be57f96343fd0ea10b647ec8f674ba1f
hHanan13 <hanan.hassona13@yahoo.com>
2023-01-19 21:07:00 +0200

    package commit

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -13,30 +13,30 @@
                     <div class="package__wrapper">
                         <div class="packTitle">
                             <img src="{{ asset('images/wrd.png') }}" alt="" class="packImgThumb">
-                            <span>Emerging Restaurant Package</span>
+                            <span>@lang('lang.Emerging Restaurant Package')</span>
                         </div>
                         <ol class="packageCont">
-                            <li> Establishing the restaurant according to the target audience.</li>
-                            <li> Designing the identity of the restaurant, packaging and store.</li>
-                            <li> Ad design and photography of advertising content.</li>
-                            <li> Menu engineering and design.</li>
-                            <li> Create your online store.</li>
-                            <li> Integration with multiple delivery platforms.</li>
-                            <li> Ads management for your store and your account on delivery platforms.</li>
-                            <li> Administrative and operational support. 9. Operational financing.</li>
-                            <li> operational financial</li>
-                            <li> your restaurant from and to.</li>
+                            <li> @lang('lang.Establishing the restaurant according to the target audience.')</li>
+                            <li> @lang('lang.Designing the identity of the restaurant, packaging and store.')</li>
+                            <li> @lang('lang.Ad design and photography of advertising content.')</li>
+                            <li> @lang('lang.Menu engineering and design.')</li>
+                            <li> @lang('lang.Create your online store.')</li>
+                            <li> @lang('lang.Integration with multiple delivery platforms.')</li>
+                            <li> @lang('lang.Ads management for your store and your account on delivery platforms.')</li>
+                            <li> @lang('lang.Administrative and operational support.')</li>
+                            <li> @lang('lang.operational financial')</li>
+                            <li> @lang('lang.your restaurant from and to.')</li>
                         </ol>
                         <div class="flexEndPack">
-                            <p>Launch your restaurant now and start selling now.</p>
-                            <a href="{{ route('payment.create') }}" class="modal__btn next__btn my-0">next</a>
+                            <p>@lang('lang.Launch your restaurant now and start selling now.')</p>
+                            <a href="{{ route('payment.create') }}" class="modal__btn next__btn my-0">@lang('lang.next')</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="contact__info p__absolute">
-            <h3 class="contact__title">contact info</h3>
+            <h3 class="contact__title">@lang('lang.contact info')</h3>
             <div class="contact__card">
                 <span class="phone_icon info__icon"></span>
                 <a href="#" class="info__num">0000 000 000 000</a>
--- resources/views/propackage.blade.php
+++ resources/views/propackage.blade.php
@@ -13,26 +13,26 @@
                     <div class="package__wrapper">
                         <div class="packTitle">
                             <img src="{{ asset('images/wrd.png') }}" alt="" class="packImgThumb">
-                            <span>Professional restaurant package</span>
+                            <span>@lang('lang.Professional restaurant package')</span>
                         </div>
                         <ol class="packageCont">
-                            <li>Create your account on the platform.</li>
-                            <li>Integration with target restaurants.</li>
-                            <li>Integration with multiple delivery platforms.</li>
-                            <li>Account management training.</li>
-                            <li>Supportive administrative support.</li>
-                            <li>Operational financing</li>
+                            <li>@lang('lang.Create your account on the platform.')</li>
+                            <li>@lang('lang.Integration with target restaurants.')</li>
+                            <li>@lang('lang.Integration with multiple delivery platforms.')</li>
+                            <li>@lang('lang.Account management training.')</li>
+                            <li>@lang('lang.Supportive administrative support.')</li>
+                            <li>@lang('lang.Operational financing')</li>
                         </ol>
                         <div class="flexEndPack">
-                            <p>Join and increase your restaurant sales now</p>
-                            <a href="{{ route('payment.create') }}" class="modal__btn next__btn my-0 mt__sm20">next</a>
+                            <p>@lang('lang.Join and increase your restaurant sales now')</p>
+                            <a href="{{ route('payment.create') }}" class="modal__btn next__btn my-0 mt__sm20">@lang('lang.next')</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="contact__info p__absolute">
-            <h3 class="contact__title">contact info</h3>
+            <h3 class="contact__title">@lang('lang.contact info')</h3>
             <div class="contact__card">
                 <span class="phone_icon info__icon"></span>
                 <a href="#" class="info__num">0000 000 000 000</a>
