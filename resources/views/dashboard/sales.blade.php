5f9aad21dbb4dba3bab8df80947f0d130396bb5e
hHanan13 <hanan.hassona13@yahoo.com>
2023-03-12 03:18:02 +0200

    sales update

--- resources/views/dashboard/sales.blade.php
+++ resources/views/dashboard/sales.blade.php
@@ -20,7 +20,7 @@
                     </li>
                 </ul>
                 <div class="tab-content"  id="myTabinner">
-                    hanan
+                    
                     <div class="current_wrapper tab-pane fade in active show" role="tabpanel" id="current_wrapper">
                         <div class="table__wrapper">
                             <div class="dashboard__table">
